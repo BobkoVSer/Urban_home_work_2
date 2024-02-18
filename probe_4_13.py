7f6b6e9d5466cfa58d10c69bae276929c5072968
BobkoVSer <156786477+BobkoVSer@users.noreply.github.com>
2024-02-18 21:59:28 +0700

    Add files via upload

---
+++ probe_4_13.py
@@ -0,0 +1,11 @@
+class House:
+    def __unit__(self):
+        self.numberOFfloors = 0
+    def __unit__(set):
+        set.new_numberOFfloors = numberOFfloors +1
+house = House()
+for numberOFfloors in range(0, 10):
+    print('Текущий этаж дома равен:', numberOFfloors+1)
+
+
+
